5d02cc301a24c5350fa58a7c80e77e92e61ef4d3
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-08-10 11:19:42 -0500

    added build for realz

---
+++ client/build/precache-manifest.b2695f2fbf2622441ac8086c81fc4bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bc7c156e483302bbea477a85201392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0030d2d135eff6a69c6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7b546dc205474d48f73b",
+    "url": "/static/css/main.23c3f2d4.chunk.css"
+  },
+  {
+    "revision": "e0030d2d135eff6a69c6",
+    "url": "/static/js/2.479b1435.chunk.js"
+  },
+  {
+    "revision": "7b546dc205474d48f73b",
+    "url": "/static/js/main.cf60a977.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
